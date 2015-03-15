4f1fc29f352ad95586c65d3fb2f8d07ab75efee3
linkyndy <linkyndy@gmail.com>
2015-03-15 16:34:55 +0200

    Added main script and track user's progress throughout the system

--- main.pl
+++ main.pl
@@ -1,3 +1,20 @@
+% Expert system should be started from here
+main :-
+  write('Which programming language should I learn first?'),
+  nl,
+  find_language(Language),
+  write(Language).
+
+
+find_language(Language) :-
+  language(Language), !.
+
+
+% Store user answers to be able to track his progress
+:- dynamic(answers/2).
+
+
+% Rules for the knowledge base
 language(python) :-
   why(for_my_kids).
 
@@ -39,17 +56,26 @@ language(objectivec) :-
   which_platform(apple).
 
 
+% Assigns an answer to questions from the knowledge base
 why(Answer) :-
-  ask(Answer, [for_my_kids, i_dont_know, make_money]).
+  answers(why, Answer).
+why(Answer) :-
+  ask(why, Answer, [for_my_kids, i_dont_know, make_money]).
 
 which_platform(Answer) :-
-  ask(Answer, [doesn_t_matter, gaming, mobile, facebook, google, windows, apple]).
+  answers(which_platform, Answer).
+which_platform(Answer) :-
+  ask(which_platform, Answer, [doesn_t_matter, gaming, mobile, facebook, google, windows, apple]).
 
 which_mobile_os(Answer) :-
-  ask(Answer, [ios, android]).
+  answers(which_mobile_os, Answer).
+which_mobile_os(Answer) :-
+  ask(which_mobile_os, Answer, [ios, android]).
 
 
-ask(Answer, Choices) :-
+% Asks the Question to the user and saves the Answer
+ask(Question, Answer, Choices) :-
   write(Choices),
   nl,
-  read(Answer).
+  read(Answer),
+  asserta(answers(Question, Answer)).
