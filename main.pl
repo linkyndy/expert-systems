9eb9a56802c9541e4be98ca5165ed0db26f628e9
linkyndy <linkyndy@gmail.com>
2015-03-23 18:45:42 +0200

    Print answers nicely
    All answers have now human-readable names.

--- main.pl
+++ main.pl
@@ -237,6 +237,113 @@ question(car) :-
   write('Auto or Manual car?'), nl.
 
 
+% Answers for the knowledge base
+answer(for_my_kids) :-
+  write('For my kids').
+
+answer(i_dont_know) :-
+  write('I don\'t know').
+
+answer(make_money) :-
+  write('Make money').
+
+answer(just_for_fun) :-
+  write('Just for fun').
+
+answer(im_interested) :-
+  write('I\'m interested').
+
+answer(improve_myself) :-
+  write('Improve myself').
+
+answer(doesn_t_matter) :-
+  write('Doesn\'t matter, I just want $$$').
+
+answer(gaming) :-
+  write('3D/Gaming').
+
+answer(mobile) :-
+  write('Mobile').
+
+answer(facebook) :-
+  write('Facebook').
+
+answer(google) :-
+  write('Google').
+
+answer(windows) :-
+  write('Windows').
+
+answer(apple) :-
+  write('Apple').
+
+answer(web) :-
+  write('Web').
+
+answer(enterprise) :-
+  write('Enterprise').
+
+answer(ios) :-
+  write('iOS').
+
+answer(android) :-
+  write('Android').
+
+answer(front_end) :-
+  write('Front-end (web interface)').
+
+answer(back_end) :-
+  write('Back-end ("brain" behind a website)').
+
+answer(startup) :-
+  write('Startup').
+
+answer(corporate) :-
+  write('Corporate').
+
+answer(im_a_fan) :-
+  write('I\'m a fan!').
+
+answer(not_bad) :-
+  write('Not Bad').
+
+answer(suck) :-
+  write('Suck').
+
+answer(yes) :-
+  write('Yes').
+
+answer(no) :-
+  write('No').
+
+answer(lego) :-
+  write('Lego').
+
+answer(play_doh) :-
+  write('Play-Doh').
+
+answer(old_ugly) :-
+  write('I\'ve an old & ugly toy, but I love it so much!').
+
+answer(easy_way) :-
+  write('The easy way').
+
+answer(best_way) :-
+  write('The best way').
+
+answer(harder_way) :-
+  write('The slightly harder way').
+
+answer(hardest_way) :-
+  write('The really hard way (but easier to pick up other languages in the future)').
+
+answer(auto) :-
+  write('Auto').
+
+answer(manual) :-
+  write('Manual').
+
+
 % Assigns an answer to questions from the knowledge base
 why(Answer) :-
   progress(why, Answer).
@@ -303,7 +410,7 @@ car(Answer) :-
 % [First|Rest] is the Choices list, Index is the index of First in Choices
 answers([], _).
 answers([First|Rest], Index) :-
-  write(Index), write(First), nl,
+  write(Index), write(' '), answer(First), nl,
   NextIndex is Index + 1,
   answers(Rest, NextIndex).
 
