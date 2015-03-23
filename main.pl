bc5975b9a2d2d29d3a5f6314723fc018a8409ce1
linkyndy <linkyndy@gmail.com>
2015-03-23 19:00:42 +0200

    Nicely print the "chosen" language with a brief description

--- main.pl
+++ main.pl
@@ -4,7 +4,7 @@ main :-
   nl,
   reset_answers,
   find_language(Language),
-  write(Language).
+  describe(Language), nl.
 
 
 find_language(Language) :-
@@ -342,6 +342,53 @@ answer(auto) :-
 
 answer(manual) :-
   write('Manual').
+
+
+% Language descriptions for the knowledge base
+describe(python) :-
+  write('Python'), nl,
+  write('Widely regarded as the best programming language for beginners'), nl,
+  write('Easiest to learn').
+
+describe(java) :-
+  write('Java'), nl,
+  write('One of the most in demand & highest paying programming languages'), nl,
+  write('Slogan: write once, work everywhere').
+
+describe(c) :-
+  write('C'), nl,
+  write('Lingua franca of programming language'), nl,
+  write('One of the oldest and most widely used language in the world').
+
+describe(cpp) :-
+  write('C++'), nl,
+  write('Complex version of C with a lot more features'), nl,
+  write('Recommended only if you have a mentor to guide you').
+
+describe(javascript) :-
+  write('JavaScript'), nl,
+  write('Most popular clients-side web scripting language'), nl,
+  write('A must learn for front-end web developer (HTML and CSS as well)').
+
+describe(csharp) :-
+  write('C#'), nl,
+  write('A popular choice for enterprise to create websites and Windows application using .NET framework'), nl,
+  write('Similar to Java in basic syntax and some features').
+
+describe(ruby) :-
+  write('Ruby'), nl,
+  write('Mostly known for its popular web framework, Ruby on Rails'), nl,
+  write('Focuses on getting things done').
+
+describe(php) :-
+  write('PHP'), nl,
+  write('Suitable for building small and simple sites within a short time frame'), nl,
+  write('Supported by almost every web hosting services with lower price').
+
+describe(objectivec) :-
+  write('Objective-C'), nl,
+  write('Primary language used by Apple for MacOSX & iOS'), nl,
+  write('Choose this if you want to focus on developing iOS or OSX apps only').
 
 
 % Assigns an answer to questions from the knowledge base
