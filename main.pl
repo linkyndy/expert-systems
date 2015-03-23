31febeda6c7e46eb3318f3279fe9e1ee95f58811
linkyndy <linkyndy@gmail.com>
2015-03-23 18:10:09 +0200

    Renamed answers to progress
    Renamed progress holder because we need answers to do something else.

--- main.pl
+++ main.pl
@@ -12,7 +12,7 @@ find_language(Language) :-
 
 
 % Store user answers to be able to track his progress
-:- dynamic(answers/2).
+:- dynamic(progress/2).
 
 
 % Clear stored user progress
@@ -20,7 +20,7 @@ find_language(Language) :-
 % or false, we fail the first and succeed with the second.
 reset_answers :-
   write('Resetting progress...'),
-  retract(answers(_, _)),
+  retract(progress(_, _)),
   fail.
 reset_answers.
 
@@ -239,63 +239,63 @@ question(car) :-
 
 % Assigns an answer to questions from the knowledge base
 why(Answer) :-
-  answers(why, Answer).
+  progress(why, Answer).
 why(Answer) :-
-  \+ answers(why, _),
+  \+ progress(why, _),
   ask(why, Answer, [for_my_kids, i_dont_know, make_money, just_for_fun, im_interested, improve_myself]).
 
 which_platform(Answer) :-
-  answers(which_platform, Answer).
+  progress(which_platform, Answer).
 which_platform(Answer) :-
-  \+ answers(which_platform, _),
+  \+ progress(which_platform, _),
   ask(which_platform, Answer, [doesn_t_matter, gaming, mobile, facebook, google, windows, apple, web, enterprise]).
 
 which_mobile_os(Answer) :-
-  answers(which_mobile_os, Answer).
+  progress(which_mobile_os, Answer).
 which_mobile_os(Answer) :-
-  \+ answers(which_mobile_os, _),
+  \+ progress(which_mobile_os, _),
   ask(which_mobile_os, Answer, [ios, android]).
 
 web(Answer) :-
-  answers(web, Answer).
+  progress(web, Answer).
 web(Answer) :-
-  \+ answers(web, _),
+  \+ progress(web, _),
   ask(web, Answer, [front_end, back_end]).
 
 want_to_work_for(Answer) :-
-  answers(want_to_work_for, Answer).
+  progress(want_to_work_for, Answer).
 want_to_work_for(Answer) :-
-  \+ answers(want_to_work_for, _),
+  \+ progress(want_to_work_for, _),
   ask(want_to_work_for, Answer, [startup, corporate]).
 
 think_about_microsoft(Answer) :-
-  answers(think_about_microsoft, Answer).
+  progress(think_about_microsoft, Answer).
 think_about_microsoft(Answer) :-
-  \+ answers(think_about_microsoft, _),
+  \+ progress(think_about_microsoft, _),
   ask(think_about_microsoft, Answer, [im_a_fan, not_bad, suck]).
 
 try_something_new(Answer) :-
-  answers(try_something_new, Answer).
+  progress(try_something_new, Answer).
 try_something_new(Answer) :-
-  \+ answers(try_something_new, _),
+  \+ progress(try_something_new, _),
   ask(try_something_new, Answer, [yes, no]).
 
 favourite_toy(Answer) :-
-  answers(favourite_toy, Answer).
+  progress(favourite_toy, Answer).
 favourite_toy(Answer) :-
-  \+ answers(favourite_toy, _),
+  \+ progress(favourite_toy, _),
   ask(favourite_toy, Answer, [lego, play_doh, old_ugly]).
 
 prefer_to_learn(Answer) :-
-  answers(prefer_to_learn, Answer).
+  progress(prefer_to_learn, Answer).
 prefer_to_learn(Answer) :-
-  \+ answers(prefer_to_learn, _),
+  \+ progress(prefer_to_learn, _),
   ask(prefer_to_learn, Answer, [easy_way, best_way, harder_way, hardest_way]).
 
 car(Answer) :-
-  answers(car, Answer).
+  progress(car, Answer).
 car(Answer) :-
-  \+ answers(car, _),
+  \+ progress(car, _),
   ask(car, Answer, [auto, manual]).
 
 
@@ -305,5 +305,5 @@ ask(Question, Answer, Choices) :-
   write(Choices),
   nl,
   read(Response),
-  asserta(answers(Question, Response)),
+  asserta(progress(Question, Response)),
   Response = Answer.
